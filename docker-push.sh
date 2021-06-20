667b0332778fbd9246c2635c755730c97bfa4c23
Sergey Razumov <sergey.razumov@nowtilus.tv>
2021-06-20 14:17:41 +0200

    tag

--- docker-push.sh
+++ docker-push.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-# branch="$(git branch --show-current)"
-# commit="$(git log -1 --format=%h)"
-branch = 'latest'
-commit = '_'
-echo $branch-$commit
+tag="$(git branch --show-current)"-"$(git log -1 --format=%h)"
+echo $tag
 myusername=${DOCKERHUB_USER}
 mypassword=${DOCKERHUB_PASS}
 echo $myusername
 echo $mypassword
 docker build -t tdd .
-docker tag tdd $myusername/tdd:$branch-$commit
+docker tag tdd $myusername/tdd:$tag
 docker login -u $myusername -p $mypassword docker.io
-docker push $DOCKERHUB_USER/tdd:$branch-$commit
+docker push $DOCKERHUB_USER/tdd:$tag
